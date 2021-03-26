8657ebd188a7053b303bda2fc7be55718e9afa8e
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-03-25 21:16:51 -0300

    finally third

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
